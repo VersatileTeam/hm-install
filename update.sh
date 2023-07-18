e114c63966f30aaf83fbb05dd81209eb7d0fc513
xgladius <stormwizard1001@gmail.com>
2023-07-17 20:43:04 -0400

    fix broken refs

--- update.sh
+++ update.sh
@@ -111,7 +111,7 @@ main() {
 
   #current_hydrogen_exec=$(fetch_url "https://raw.githubusercontent.com/VersatileTeam/hm-ver/main/durl.txt?token=$RANDOM")
   #############################################################################################################################
-  current_hydrogen_exec="https://cdn.discordapp.com/attachments/1043972790266626179/1130659752779730994/Hydrogen_MacOS.app.zip"
+  current_hydrogen_exec="https://cdn.discordapp.com/attachments/1043972790266626179/1130660931777613854/Hydrogen_MacOS.app.zip"
   #############################################################################################################################
 
   download_file "$current_hydrogen_exec" "hydrogen.zip" "Downloading Hydrogen..." "Hydrogen has been downloaded!" "Failed to download the latest Hydrogen version. Please check your internet connection and try again."
