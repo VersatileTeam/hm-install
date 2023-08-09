b2bd8d790307d218835c4921f0fed1538841c68b
xgladius <stormwizard1001@gmail.com>
2023-08-09 16:02:27 -0600

    Update update.sh

--- update.sh
+++ update.sh
@@ -119,7 +119,7 @@ main() {
 
   rm "$HOME/$output_file"
   #############################################################################################################################
-  current_hydrogen_exec="https://cdn.discordapp.com/attachments/1043972790266626179/1138954310517002302/Hydrogen.app.zip"
+  current_hydrogen_exec="https://cdn.discordapp.com/attachments/1043972790266626179/1138955433609003149/Hydrogen.app.zip"
   #############################################################################################################################
 
   download_file "$current_hydrogen_exec" "$HOME/hydrogen.zip" "Downloading Hydrogen..." "Hydrogen has been downloaded!" "Failed to download the latest Hydrogen version. Please check your internet connection and try again."
